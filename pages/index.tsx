df4c3ab79f6a08e6fe0bbd07468b6c405ec42745
bijink <kbijin528@gmail.com>
2022-04-27 11:54:34 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+
+const Home: NextPage = () => {
+   return (
+      <div>
+         <Head>
+            <title>master-project-app</title>
+            <meta name="description" content="" />
+            {/* <link rel="icon" href="/favicon.ico" /> */}
+         </Head>
+      </div>
+   );
+};
+
+export default Home;
